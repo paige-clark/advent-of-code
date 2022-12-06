596fb68de1ffd849c47fe547a689f1231a08f5c6
paige-clark <paigeclarkillustration@gmail.com>
2022-12-06 09:28:54 -0500

    Add day 6 part 1 solution

--- 2022/day-6/a.js
+++ 2022/day-6/a.js
@@ -1,15 +1,40 @@
 const fs = require("fs");
 const input = fs.readFileSync("input.txt", "utf-8").toString();
 
+const testSignal = `
+zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw
+`;
+
 const signalTuner = (inputData) => {
-  
-}
- 
+  const signal = inputData.trim();
+  let counter = 0;
+  for (let i = 0; i < signal.length; i++) {
+    let uniqueObj = {};
+    uniqueObj[signal[i]] = true;
+    uniqueObj[signal[i + 1]] = true;
+    uniqueObj[signal[i + 2]] = true;
+    uniqueObj[signal[i + 3]] = true;
+
+    const numOfKeys = Object.keys(uniqueObj).length;
+    if (numOfKeys !== 4) {
+      counter++;
+    } else if (numOfKeys === 4) {
+      counter += 4;
+      break;
+    }
+  }
+  return counter;
+};
+
+console.log(signalTuner(input));
+
 /** Tuning Trouble Part 1!
  * INPUT: a string of random characters
- * 
- * + 
- * 
+ *
+ * + establish an empty holder array
+ * + loop through each character in the signal string
+ *   + as you loop through it add
+ *
  * OUTPUT: the amount of characters that need to be processed
  *         to find a set of 4 unique characters
  */
