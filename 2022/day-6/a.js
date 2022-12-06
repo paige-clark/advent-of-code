2ebad3701717d2db4a4d3260ebe848a13318871e
paige-clark <paigeclarkillustration@gmail.com>
2022-12-06 08:59:44 -0500

    Set up day 6 files

---
+++ 2022/day-6/a.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+const input = fs.readFileSync("input.txt", "utf-8").toString();
+
+const signalTuner = (inputData) => {
+  
+}
+ 
+/** Tuning Trouble Part 1!
+ * INPUT: a string of random characters
+ * 
+ * + 
+ * 
+ * OUTPUT: the amount of characters that need to be processed
+ *         to find a set of 4 unique characters
+ */
